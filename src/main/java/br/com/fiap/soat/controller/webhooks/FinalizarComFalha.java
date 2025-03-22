020ff84639225d031f67682102a24b184efa5b5b
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-03-22 09:07:22 -0300

    Correção no webhook FinalizarComFalha

--- src/main/java/br/com/fiap/soat/controller/webhooks/FinalizarComFalha.java
+++ src/main/java/br/com/fiap/soat/controller/webhooks/FinalizarComFalha.java
@@ -1,12 +1,13 @@
 package br.com.fiap.soat.controller.webhooks;
 
+import br.com.fiap.soat.dto.FalhaDto;
 import br.com.fiap.soat.service.provider.FinalizarComFalhaService;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +24,9 @@ public class FinalizarComFalha {
 
   @Hidden
   @GetMapping(value = "/sucesso/{jobId}/{filePath}")
-  public ResponseEntity<Void> listarVideos(@PathVariable("jobId") String jobId,
-      @PathVariable("filePath") String filePath) {
+  public ResponseEntity<Void> finalizarComFalha(@RequestBody FalhaDto requisicao) {
     
-    List<ProcessamentoDto> lista = service.execute(requisicao);
+    service.processarRequisicao(requisicao);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
   }
 }
