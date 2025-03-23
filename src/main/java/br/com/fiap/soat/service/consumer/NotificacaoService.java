b53e50d07e63c319789bab858db216ad64990b25
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-03-23 15:58:41 -0300

    Inclusão do header na requisição em NotificacaoService

--- src/main/java/br/com/fiap/soat/service/consumer/NotificacaoService.java
+++ src/main/java/br/com/fiap/soat/service/consumer/NotificacaoService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,12 +35,15 @@ public class NotificacaoService {
     LoggerAplicacao.info("URL: " + url);
     LoggerAplicacao.info("Dados do email: " + dadosEmail);
 
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<EmailDto> httpEntity = new HttpEntity<>(dadosEmail, headers);
 
     try {
       restTemplate.exchange(
           url,
           HttpMethod.POST,
-          new HttpEntity<>(dadosEmail),
+          httpEntity,
           new ParameterizedTypeReference<Void>() {});
 
     } catch (Exception e) {
