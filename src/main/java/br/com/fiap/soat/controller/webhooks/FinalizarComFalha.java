8368e89dec1acfe5cb847e5430ef3087421d1837
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-03-23 15:17:45 -0300

    Correção no mapeamento de endpoint

--- src/main/java/br/com/fiap/soat/controller/webhooks/FinalizarComFalha.java
+++ src/main/java/br/com/fiap/soat/controller/webhooks/FinalizarComFalha.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +24,7 @@ public class FinalizarComFalha {
   }
 
   @Hidden
-  @GetMapping(value = "/falha")
+  @PatchMapping(value = "/falha")
   public ResponseEntity<Void> finalizarComFalha(@RequestBody FalhaDto requisicao) {
 
     LoggerAplicacao.info("Recebeu uma falha: " + requisicao.toString());
